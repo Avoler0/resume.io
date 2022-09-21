c8ec9b45b706bcb1309ac26ab397c0b87a73dfeb
Avoler0 <jys1391@gmail.com>
2022-09-21 18:45:46 +0900

    Feat: github url 추가

--- src/Component/Project.tsx
+++ src/Component/Project.tsx
@@ -10,10 +10,10 @@ export default function Project(){
     <div> 
       <H3>Project</H3>
       <Content>
-        {/* <ProjectCard project="wewi.gg" person="개인" image={image.wewigg} Text={project.wewigg} git={github.wewigg}/> */}
+        <ProjectCard project="wewi.gg" person="개인" image={image.wewigg} Text={project.wewigg} git={github.wewigg}/>
         {/* <ProjectCard project="크라우드 워커 지원 페이지" person="5인" image={image.snplab} Text={project.snplab} git={github.snplab}/> */}
         {/* <ProjectCard project="영화 정보 사이트" person="5인" image={image.movieInfo} Text={project.movieInfo} git={github.movieInfo}/> */}
-        {/* <ProjectCard project="호텔 예약 사이트" person="5인" image={image.tripbtoz} Text={project.tripbtoz} git={github.tripbtoz}/> */}
+        <ProjectCard project="호텔 예약 사이트" person="5인" image={image.tripbtoz} Text={project.tripbtoz} git={github.tripbtoz}/>
       </Content>
     </div>
   )
